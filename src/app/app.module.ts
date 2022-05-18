1567846df92854c773c2d642b25cf484a0df7442
VitaliiKosyk <kostamtik@gmail.com>
2022-05-18 18:49:31 +0300

    lesson 2 hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "home-page", component: HomeComponent},
+      {path: "users-page",
+        component: UsersComponent,
+        children:[
+          {path: "user-details/:id", component: UserDetailComponent}
+        ]
+      },
+      // {path: "user-details/:id", component: UserDetailComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../interfaces/user.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _url = 'https://jsonplaceholder.typicode.com/users'
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(this._url);
+  }
+
+  geByID(id: number): Observable<IUser> {
+    return this.http.get<IUser>(this._url + '/' + id);
+  }
+}
