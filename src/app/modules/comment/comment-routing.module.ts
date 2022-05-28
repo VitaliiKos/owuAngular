c97e3e690f169570b7b26c83d47edb12811656ce
VitaliiKosyk <kostamtik@gmail.com>
2022-05-28 12:45:53 +0300

    lesson 4 hw edit

--- src/app/modules/comment/comment-routing.module.ts
+++ src/app/modules/comment/comment-routing.module.ts
@@ -4,11 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {CommentsComponent} from "./components/comments/comments.component";
 import {CommentDetailsComponent} from "./components/comment-details/comment-details.component";
 import {CommentResolver} from "./services";
+import {CommentGuardGuard} from "./guards/comment-guard.guard";
 
 
 const routes: Routes = [
   {
-    path: '', component: CommentsComponent, children: [
+    path: '', component: CommentsComponent,
+    canActivate:[CommentGuardGuard],
+    children: [
       {path: ':id', component: CommentDetailsComponent, resolve: {data: CommentResolver}}
     ]
   }
